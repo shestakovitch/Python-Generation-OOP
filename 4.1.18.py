f8563d13f30778417e861fc3c6067ec99fb40672
shestakovitch <shestakovitch@gmail.com>
2024-07-10 14:55:02 +0200

    Класс PiggyBank

---
+++ 4.1.18.py
@@ -0,0 +1,9 @@
+"""Класс PiggyBank
+Реализуйте пустой класс PiggyBank, а затем создайте экземпляр этого класса и присвойте его переменной money_box."""
+
+
+class PiggyBank:
+    pass
+
+
+money_box = PiggyBank()
