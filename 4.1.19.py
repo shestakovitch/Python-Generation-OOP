45d64957a2d6ad9c7ae1d7d7e7b46a9bff584539
shestakovitch <shestakovitch@gmail.com>
2024-07-10 14:56:31 +0200

    Класс PiggyBank

---
+++ 4.1.19.py
@@ -0,0 +1,22 @@
+"""Класс PiggyBank
+Реализуйте пустой класс PiggyBank, а затем создайте два экземпляра этого класса и присвойте их переменным money_box1 и
+money_box2.
+
+Экземпляр money_box1 должен иметь:
+
+атрибут coins со значением 10
+Экземпляр money_box2 должен иметь:
+
+атрибут coins со значением 15
+атрибут color со значением 'pink'"""
+
+
+class PiggyBank:
+    pass
+
+
+money_box1 = PiggyBank()
+money_box2 = PiggyBank()
+money_box1.coins = 10
+money_box2.coins = 15
+money_box2.color = 'pink'
