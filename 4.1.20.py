e197b05c0ea5d31b20df84ee3eee611ad02974b0
shestakovitch <shestakovitch@gmail.com>
2024-07-10 14:57:38 +0200

    Класс PiggyBank

---
+++ 4.1.20.py
@@ -0,0 +1,15 @@
+"""Класс PiggyBank
+Реализуйте класс PiggyBank, а затем создайте экземпляр этого класса и присвойте его переменной money_box.
+
+Класс PiggyBank должен иметь:
+
+атрибут content со значением 'coins'
+атрибут alternate_name со значением 'penny bank'"""
+
+
+class PiggyBank:
+    content = 'coins'
+    alternate_name = 'penny bank'
+
+
+money_box = PiggyBank()
