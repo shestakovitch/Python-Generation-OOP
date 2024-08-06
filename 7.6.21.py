2a262380735be2a1f7d3fc899f70ea07baf14eb5
shestakovitch <shestakovitch@gmail.com>
2024-08-06 20:13:46 +0200

    Иерархия классов 🔠

---
+++ 7.6.21.py
@@ -0,0 +1,50 @@
+"""Иерархия классов 🔠
+С помощью наследования и приведенной ниже схемы постройте иерархию пустых классов:
+                                    A
+                                 /  |  \
+                                B   C   D
+                                \      /
+                                    E
+"""
+
+
+class A:
+    pass
+
+
+class B(A):
+    pass
+
+
+class C(A):
+    pass
+
+
+class D(A):
+    pass
+
+
+class E(B, D):
+    pass
+
+
+# Sample Input 1:
+#
+# print(issubclass(E, B))
+# print(issubclass(E, C))
+# print(issubclass(E, D))
+# Sample Output 1:
+#
+# True
+# False
+# True
+# Sample Input 2:
+#
+# print(issubclass(B, A))
+# print(issubclass(C, A))
+# print(issubclass(D, A))
+# Sample Output 2:
+#
+# True
+# True
+# True
